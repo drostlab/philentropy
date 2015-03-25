7dff0fc71337f2530699f36fe41a28fca611b469
HajkD <hajkdrost@googlemail.com>
2015-03-25 16:13:38 +0100

    adding cpp distance function divergence_sq()

--- src/distances.cpp
+++ src/distances.cpp
@@ -789,7 +789,27 @@ double prob_symm_chi_sq(NumericVector P, NumericVector Q){
 
 
 
-
+//' @export
+// [[Rcpp::export]]
+double divergence_sq(NumericVector P, NumericVector Q){
+        
+        int    P_len      = P.size();
+        int    Q_len      = Q.size();
+        double dist      = 0.0;
+        
+        if (P_len != Q_len){
+                Rcpp::stop("The vectors you are comparing do not have the same length!");
+        }
+        
+        for(int i = 0; i < P_len; i++){
+                
+                dist = dist + (pow(P[i] - Q[i], 2) / pow(P[i] + Q[i], 2));
+                
+        }
+        
+        return 2.0 * dist;
+        
+}
 
 
 
