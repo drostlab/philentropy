5cd5d431b47f1e92d8069c7640ab8e8bbae0f8ed
HajkD <hajkdrost@googlemail.com>
2015-04-04 18:17:15 +0200

    adding Licese information to all code files

--- R/distance.R
+++ R/distance.R
@@ -1,3 +1,23 @@
+#  Part of the philentropy package
+#
+#  Copyright (C) 2015 Hajk-Georg Drost
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+
+
+
 #' @title Distance between Probability Density Functions
 #' @description This functions computes the distance/dissimilarity between two probability density functions.
 #' @param x a numeric vector (probability density function).
