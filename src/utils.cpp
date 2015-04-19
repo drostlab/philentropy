a2db890a0f3c8106d37810ee5476854e073e20b8
HajkD <hajkdrost@googlemail.com>
2015-04-19 17:03:30 +0200

    update vectorSum()

--- src/utils.cpp
+++ src/utils.cpp
@@ -6,11 +6,11 @@ using namespace std;
 //' @export
 // [[Rcpp::export]]
 double vectorSum(NumericVector x) {
-        
         //if(is_na(x)){
         //       Rcpp::stop("Your input vector includes NA values...");
         // }
         
+        // http://gallery.rcpp.org/articles/parallel-vector-sum/
         return std::accumulate(x.begin(), x.end(), 0.0);
 }
 
