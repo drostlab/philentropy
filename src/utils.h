254f9672a6d52ed52a7710bc0c5c25cdf517a6fb
HajkD <hajkdrost@googlemail.com>
2015-04-22 11:10:40 +0200

    add new function as_data_frame() to utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,10 +1,15 @@
-#include <Rcpp.h>
-#include <algorithm>
-using namespace Rcpp;
-using namespace std;
+
 
 #ifndef philentropy_UTILS_H
 #define philentropy_UTILS_H philentropy_UTILS_H
+
+
+#include <Rcpp.h>
+#include <algorithm>
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::interfaces(r, cpp)]]
+
 
 //[[Rcpp::export]]
 Rcpp::NumericMatrix as_matrix(Rcpp::DataFrame x) {
@@ -19,7 +24,20 @@ Rcpp::NumericMatrix as_matrix(Rcpp::DataFrame x) {
 
 //' @export
 // [[Rcpp::export]]
-double vectorSum(NumericVector x) {
+Rcpp::DataFrame as_data_frame(Rcpp::NumericMatrix mat){
+ 
+ //int nRows=mat.nrow();  
+  Rcpp::DataFrame y;
+  for (int i=0; i<mat.ncol();i++) {
+       y[i] = mat(Rcpp::_,i);
+  }  
+  return y;
+}
+
+
+//' @export
+// [[Rcpp::export]]
+double vectorSum(Rcpp::NumericVector x) {
         //if(is_na(x)){
         //       Rcpp::stop("Your input vector includes NA values...");
         // }
@@ -31,16 +49,16 @@ double vectorSum(NumericVector x) {
 //' @export
 // [[Rcpp::export]]
 SEXP sum2( SEXP x_ ){
-   NumericVector x(x_) ;
+   Rcpp::NumericVector x(x_) ;
    double res = sum( x ) ;
-   return wrap( res ) ;
+   return Rcpp::wrap( res ) ;
 }
 
 
- double sum3(NumericVector x) {
+ double sum3(Rcpp::NumericVector x) {
     double total = 0;
     
-    NumericVector::iterator it;
+    Rcpp::NumericVector::iterator it;
     for(it = x.begin(); it != x.end(); ++it) {
       total += *it;
     }
