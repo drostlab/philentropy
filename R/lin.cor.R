01e6c6909960e0233829e864eb5ddecce9bcf374
HajkD <hajkdrost@googlemail.com>
2015-04-26 13:48:08 +0200

    adding documentation for function lin.cor()

--- R/lin.cor.R
+++ R/lin.cor.R
@@ -1,4 +1,28 @@
-lin.corr <- function(x,y, method = "pearson"){
+#' @title Linear Correlation
+#' @description This function computed the linear correlation between two vectors or a correlation matrix for an input matrix.
+#' 
+#' The following methods to compute linear correlations are implemented in this function:
+#'  
+#'  @param x a numeric \code{vector}, \code{matrix}, or \code{data.frame}.
+#'  @param y a numeric \code{vector} that should be correlated with \code{x}.
+#'  @author Hajk-Georg Drost
+#'  @details
+#'  
+#'    \itemize{
+#'   \item \code{method = "pearson"} : Pearson's correlation coefficient (centred).
+#'   \item \code{method = "pearson2"} : Pearson's uncentred correlation coefficient.
+#'   \item \code{method = "sq_pearson"} . Squared Pearson's correlation coefficient.
+#'   \item \code{method = "kendall"} : Kendall's correlation coefficient.
+#'   \item \code{method = "spearman"} : Spearman's correlation coefficient.
+#'   } 
+#'   
+#'  Further Details:
+#'   
+#'  \itemize{
+#'  \item \emph{Pearson's correlation coefficient (centred)} : 
+#'  } 
+#'    @export 
+lin.corr <- function(x,y = NULL, method = "pearson"){
         
         cor.coef <- vector("numeric",1)
                 
