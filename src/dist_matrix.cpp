86bb356c8a9de8a2d5a5067b81f16112d04cd90e
HajkD <hajkdrost@googlemail.com>
2015-04-27 12:01:09 +0200

    remodeling the structure of DistMatrixWithoutUnitMAT() to column centric distance computations of input matrices

--- src/dist_matrix.cpp
+++ src/dist_matrix.cpp
@@ -32,21 +32,21 @@ Rcpp::NumericMatrix DistMatrixWithoutUnitDF(Rcpp::DataFrame distsDF, Rcpp::Funct
         return dist_matrix;
 }
 
+
 //' @export
 // [[Rcpp::export]]
 Rcpp::NumericMatrix DistMatrixWithoutUnitMAT(Rcpp::NumericMatrix dists, Rcpp::Function DistFunc, bool testNA){
-// http://stackoverflow.com/questions/27391472/passing-r-function-as-parameter-to-rcpp-function
-       //Rcpp::NumericMatrix dists = Rcpp::clone(distsMT);
-        int nrow = dists.nrow();
-        double dist_value;
-        Rcpp::NumericMatrix dist_matrix(nrow,nrow);
+        int ncols = dists.ncol();
+        double dist_value = 0.0;
+        Rcpp::NumericMatrix dist_matrix(ncols,ncols);
         // http://stackoverflow.com/questions/23748572/initializing-a-matrix-to-na-in-rcpp
         std::fill( dist_matrix.begin(), dist_matrix.end(), Rcpp::NumericVector::get_na() );
-        for (int i = 0; i < nrow; i++){
-                for (int j = 0; j < nrow; j++){
+       
+        for (int i = 0; i < ncols; i++){
+                for (int j = 0; j < ncols; j++){
                         if(Rcpp::NumericVector::is_na(dist_matrix(i,j))){
                                 
-                                dist_value = Rcpp::as<double>(DistFunc(dists(i,Rcpp::_),dists(j,Rcpp::_), testNA));
+                                dist_value = Rcpp::as<double>(DistFunc(dists(Rcpp::_,i),dists(Rcpp::_,j), testNA));
                                 dist_matrix(i,j) = dist_value;
                                 dist_matrix(j,i) = dist_value;
                         }
