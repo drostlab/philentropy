ded6df8213bdcbebb8ff62197f3994173bd292d7
HajkD <hajkdrost@googlemail.com>
2015-06-15 23:18:47 +0200

    remove documentation for valid.distr()

--- R/utils.R
+++ R/utils.R
@@ -23,8 +23,7 @@ NULL
 
 
 
-#' @title Check the validity of input probability distributions
-
+# @title Check the validity of input probability distributions
 valid.distr <- function(x){
         
         sapply(x, function(vec) if(!dplyr::between(vec,0,1)) stop("Your probability values are not between: [0,1].", .call = FALSE))
