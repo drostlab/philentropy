6c1fedf526461a2266af487fe683b4001fa89323
HajkD <hajkdrost@googlemail.com>
2015-06-15 23:17:55 +0200

    adding documentation for arguments of binned.kernel.est()

--- R/binned.kernel.est.R
+++ R/binned.kernel.est.R
@@ -1,14 +1,19 @@
 #' @title Kernel Density Estimation
 #' @description This function implements an interface to the kernel density estimation functions provided by the \pkg{KernSmooth} package.
 #' @param data a numeric vector containing the sample on which the kernel density estimate is to be constructed.
-#' @param kernel 
-#' @param bandwidth
-#' @param canonical
-#' @param scalest
-#' @param level
-#' @param gridsize
-#' @param range.data
-#' @param truncate
+#' @param kernel character string specifying the smoothing kernel
+#' @param bandwidth the kernel bandwidth smoothing parameter.
+#' @param canonical a logical value indicating whether canonically scaled kernels should be used
+#' @param scalest estimate of scale. 
+#' \itemize{
+#'  \item \code{"stdev"} - standard deviation is used.
+#'  \item \code{"iqr"} - inter-quartile range divided by 1.349 is used.
+#'  \item \code{"minim"} - minimum of \code{"stdev"} and \code{"iqr"} is used.
+#' }
+#' @param level number of levels of functional estimation used in the plug-in rule.
+#' @param gridsize the number of equally-spaced points over which binning is performed to obtain kernel functional approximation.
+#' @param range.data vector containing the minimum and maximum values of \code{data} at which to compute the estimate. The default is the minimum and maximum data values.
+#' @param truncate logical value indicating whether data with x values outside the range specified by \code{range.data} should be ignored.
 #' @author Hajk-Georg Drost
 #' 
 #' @references
