be0e6c480a54b6f49887a4e41291d654549667a6
HajkD <hajkdrost@googlemail.com>
2015-06-16 23:51:43 +0200

    adding unit test for distance matrix computations based on divergence distances

--- tests/testthat/test-distances.R
+++ tests/testthat/test-distances.R
@@ -888,8 +888,19 @@ test_that("distance(method = 'prob_symm') computes the correct distance value in
 
 test_that("distance(method = 'divergence') computes the correct distance value.", {
         
-        expect_equal(as.vector(philentropy::distance(rbind(P, Q), method = "divergence")), 2 * sum(((P) - (Q))^2 / ((P) + (Q))^2))
+        test_divergence_dist <- function(P,Q){
+                2 * sum(((P) - (Q))^2 / ((P) + (Q))^2)
+        }
         
+        expect_equal(as.vector(philentropy::distance(rbind(P, Q), method = "divergence")),
+                     test_divergence_dist(P,Q))
+        
+        # test correct computation of distance matrix
+        distMat <- rbind(rep(0.2,5),rep(0.1,5), c(5,1,7,9,5))
+        dist.vals <- distance(distMat, method = "divergence")
+        
+        expect_equal(dist.vals[lower.tri(dist.vals, diag = FALSE)],
+                     test_dist_matrix(distMat, FUN = test_divergence_dist))
 })
 
 
