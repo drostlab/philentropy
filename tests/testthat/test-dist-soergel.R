c728555738517b3afe5c4d84cbd2cf2faeef151f
HajkD <hgd23@cam.ac.uk>
2018-05-22 11:44:59 +0100

    add new unit test

---
+++ tests/testthat/test-dist-soergel.R
@@ -0,0 +1,59 @@
+context("Test implementation of soergel distance ...")
+
+
+P <- 1:10 / sum(1:10)
+Q <- 20:29 / sum(20:29)
+V <- -10:10
+W <- -20:0
+
+# function to test distance matrix functionality
+# for different distance measures
+test_dist_matrix <- function(x, FUN) {
+  dist.fun <- match.fun(FUN)
+  res.dist.matrix <- matrix(NA_real_, nrow(x), nrow(x))
+  
+  for (i in 1:nrow(x)) {
+    for (j in 1:nrow(x)) {
+      res.dist.matrix[i, j] <- dist.fun(x[i,], x[j,])
+    }
+  }
+  return(res.dist.matrix[lower.tri(res.dist.matrix, diag = FALSE)])
+}
+
+
+test_soergel_dist <- function(P, Q) {
+  sum(abs((P) - (Q))) / sum(apply(rbind(P, Q), 2, max))
+}
+
+test_that("distance(method = 'soergel') computes the correct distance value.",
+          {
+            expect_equal(as.vector(philentropy::distance(rbind(P, Q), method = "soergel")),
+                         test_soergel_dist(P, Q))
+            
+            # test correct computation of distance matrix
+            distMat <-
+              rbind(rep(0.2, 5), rep(0.1, 5), c(5, 1, 7, 9, 5))
+            dist.vals <- distance(distMat, method = "soergel")
+            
+            expect_equal(dist.vals[lower.tri(dist.vals, diag = FALSE)],
+                         test_dist_matrix(distMat, FUN = test_soergel_dist))
+            
+          })
+
+
+test_that("Correct soergel distance is computed when vectors contain 0 values ...",
+          {
+            P1 <- c(1, 0)
+            P2 <- c(0.5, 0.5)
+            Q1 <- c(0.5, 0.5)
+            Q2 <- c(1, 0)
+            
+            distMat <-
+              rbind(P1, Q1, P2, Q2)
+            dist.vals <-
+              distance(distMat, method = "soergel")
+            
+            expect_equal(dist.vals[lower.tri(dist.vals, diag = FALSE)],
+                         test_dist_matrix(distMat, FUN = test_soergel_dist))
+            
+          })
